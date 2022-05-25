f11ade94a03b27561b4b05f462e63726db529829
Hk <hk@Ages-MacBook-Pro-HK.local>
2022-05-25 18:36:14 +0530

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+// const userRoute = require('./user');
+// const applicationRoute = require('./application');
+
+const router = express.Router();
+
+// router.use('/user', userRoute);
+// router.use('/application', applicationRoute)
+
+module.exports = router;
